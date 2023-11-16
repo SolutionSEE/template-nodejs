c1fe00f464fa2d6e1f7db2d33a21e3e9e0356670
toivo <toivo.ranta@screen1.io>
2023-11-16 04:47:44 +0000

    feat: added

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-const express = require('express');
-const app = express();
+import express from 'express'
+import { OpenAI } from "langchain/llms/openai";
+import { SqlDatabase } from "langchain/sql_db";
+import { createSqlAgent, SqlToolkit } from "langchain/agents/toolkits/sql";
+import { DataSource } from "typeorm";
+import { config } from "dotenv";
+config();
 const port = process.env.PORT ?? 3000;
-
+const app = express();
 app.use(express.static('public'))
 
-app.get('*', (req, res) => {
-    res.redirect('/');
+app.get('*', async (req, res) => {
+    try{
+      const datasource = new DataSource({
+        type: "sqlite",
+        database: "./northwind.db",
+      });
+      const db = await SqlDatabase.fromDataSourceParams({
+        appDataSource: datasource,
+      });
+      const model = new OpenAI({ temperature: 0 });
+      const toolkit = new SqlToolkit(db, model);
+      const executor = createSqlAgent(model, toolkit);
+    
+      const input = `List the Order Details`;
+    
+      console.log(`Executing with input "${input}"...`);
+    
+      const result = await executor.call({ input });
+    
+      console.log(`Got output ${result.output}`);
+    
+      console.log(
+        `Got intermediate steps ${JSON.stringify(
+          result.intermediateSteps,
+          null,
+          2
+        )}`
+      );
+    
+      await datasource.destroy();
+       
+    }
+    catch(err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+     res.redirect('/');
 })
 
 app.listen(port, () => {
